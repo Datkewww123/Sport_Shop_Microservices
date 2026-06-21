4c8803f5a886dd61c5d58fa609a34f4da901edae
Đạt Nguyễn <nguyendatz567@gmail.com>
2026-06-21 14:19:48 +0700

    update catalog routes index

--- catalog-service/app/routes/index.js
+++ catalog-service/app/routes/index.js
@@ -4,5 +4,8 @@ const router   = express.Router();
 router.use('/brands',     require('./brands'));
 router.use('/categories', require('./categories'));
 router.use('/products',   require('./products'));
+router.use('/reviews',    require('./reviews'));
+router.use('/wishlist',   require('./wishlist'));
+router.use('/news',       require('./news'));
 
 module.exports = router;
