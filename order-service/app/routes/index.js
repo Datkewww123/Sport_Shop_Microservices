622444d1f260a2f4964618d972bc015f13ee9d61
Đạt Nguyễn <nguyendatz567@gmail.com>
2026-06-21 14:19:49 +0700

    update order routes index

--- order-service/app/routes/index.js
+++ order-service/app/routes/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const router  = express.Router();
 router.use('/orders', require('./orders'));
+router.use('/promotions', require('./promotions'));
+router.use('/admin/orders', require('./adminOrders'));
 module.exports = router;
