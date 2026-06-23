0a7e2f9ee1ddc5c843e358ab24664b2f37ffd63f
Đạt Nguyễn <nguyendatz567@gmail.com>
2026-06-23 15:21:44 +0700

    refactor: update init.sql

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS soap_identity_db
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS soap_catalog_db
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE DATABASE IF NOT EXISTS soap_order_db
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
